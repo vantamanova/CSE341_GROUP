0dac94f3b712de6a6aca74474299052ea0328741
Vera Antamanova <vantamanova@gmail.com>
2025-09-05 11:41:00 +0400

    W01-Group-Part2

---
+++ db/connect.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { MongoClient } = require('mongodb');
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      // explicitly select your database name here
+      _db = client.db('341BD');   // 👈 use your DB name from Atlas
+      console.log('Database connected!');
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
+};
+
+module.exports = {
+  initDb,
+  getDb,
+};
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
 
 const professionalRoutes = require('./routes/professional');
 
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 8080;
-
-// route to get all data
 app.use(cors());
 app.use('/professional', professionalRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
